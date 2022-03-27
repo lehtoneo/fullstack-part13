a3066f31ac2f1f1750e57bf27d9ea5c16e4772df
Ossi Lehtonen <ossi.j.lehtonen@helsinki.fi>
2022-03-27 15:42:20 +0300

    23

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-
+const { Op } = require('sequelize')
 const { User, Blog, ReadingList } = require('../models')
 
 router.get('/', async (req, res) => {
@@ -18,6 +18,18 @@ router.post('/', async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
+  const where = {}
+
+  if (req.query.read !== undefined) {
+    const { read } = req.query
+    if (read !== 'true' && read !== 'false') {
+      throw { status: 400, message: 'Read has to be "true" or "false"'}
+    }
+    where.read = {
+      [Op.eq]: req.query.read === 'true'
+    }
+  }
+
   const user = await User.findByPk(req.params.id, {
     include:[{
       model: Blog,
@@ -29,7 +41,8 @@ router.get('/:id', async (req, res) => {
       attributes: { exclude: ['userId', 'reading_lists']},
       through: {
         attributes: ['read', 'id'],
-        as: 'readingList'
+        as: 'readingList',
+        where
       },
     }
   ]
